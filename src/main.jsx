785f7bda4dda7741fac31b2971d436dea610ab27
Sam Bowen <samhbowen@gmail.com>
2025-03-06 18:22:24 -0800

    init commit

---
+++ src/main.jsx
@@ -0,0 +1,18 @@
+// src/main.jsx
+
+import React from 'react'
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import { BrowserRouter } from 'react-router' // add import for BrowserRouter
+import './index.css'
+import App from './App.jsx'
+
+// Wrap the App component with the BrowserRouter component to enable
+// enable route handling throughout your application.
+createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </React.StrictMode>,
+)
